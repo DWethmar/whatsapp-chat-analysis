b1b6d9cfa1fedd2bd6f601e2d0e4b429776c22ad
Dennis Wethmar <denniswethmar@gmail.com>
2020-04-08 16:15:08 +0200

    Typo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <header>
       <h1>WhatsApp Chat Analysis</h1>
       <p>Upload a Whatsapp chat archive to view the messages and stats.</p>
-      <p>Everything is done in the browser, so no data is send to a server. But don't use this is you have <b>any</b> privacy concerns.</p>
+      <p>Everything is done in the browser, so no data is sent to a server. But don't use this is you have <b>any</b> privacy concerns.</p>
       <p>Click <a href="https://faq.whatsapp.com/en/android/23756533/">here </a> to find out how to export a chat on android.</p>
     </header>
   );
