25288560c2b61584e074d8beecf2b69a0b7fc1d4
Dennis Wethmar <denniswethmar@gmail.com>
2020-04-08 15:38:59 +0200

    Updates

---
+++ precache-manifest.1c1d19ef92e354226a1cf6ed624fba09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b1c31bf443ed7fa6b5c127d1e589cc",
+    "url": "/whatsapp-chat-analysis/index.html"
+  },
+  {
+    "revision": "044d96b57935a1e75e72",
+    "url": "/whatsapp-chat-analysis/static/css/main.12997a04.chunk.css"
+  },
+  {
+    "revision": "7f6c3730943ff840acd1",
+    "url": "/whatsapp-chat-analysis/static/js/2.d4e1452d.chunk.js"
+  },
+  {
+    "revision": "0fd412b72f2e5c023db37b093ba1ebc3",
+    "url": "/whatsapp-chat-analysis/static/js/2.d4e1452d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044d96b57935a1e75e72",
+    "url": "/whatsapp-chat-analysis/static/js/main.9fb163f1.chunk.js"
+  },
+  {
+    "revision": "3ab79d87171c7dd96977",
+    "url": "/whatsapp-chat-analysis/static/js/runtime-main.b02445a8.js"
+  }
+]);
