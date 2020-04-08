64f00ae4d1a47e6df9189dffaf68924b13fdd797
Dennis Wethmar <denniswethmar@gmail.com>
2020-04-08 17:06:05 +0200

    Yes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
       <p>Upload a Whatsapp chat archive to view the messages and stats.</p>
       <p>Everything is done in the browser, so no data is sent to a server. But don't use this is you have <b>any</b> privacy concerns.</p>
       <p>Click <a href="https://faq.whatsapp.com/en/android/23756533/">here </a> to find out how to export a chat on android.</p>
-      <p><a download href={process.env.PUBLIC_URL + "/_chat.txt"}>Download example Whatsapp chat</a></p>
+      <p><a download href="https://raw.githubusercontent.com/DWethmar/whatsapp-chat-analysis/master/public/_chat.txt">Download example Whatsapp chat</a></p>
     </header>
   );
 };
