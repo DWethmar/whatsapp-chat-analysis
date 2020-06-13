eeb3ae03183184fcc985cfae431a9b56b63b5b94
Dennis Wethmar <denniswethmar@gmail.com>
2020-06-13 16:49:14 +0200

    Fixed typos

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,9 +5,23 @@ export const Header = () => {
     <header>
       <h1>WhatsApp Chat Analysis</h1>
       <p>Upload a Whatsapp chat archive to view the messages and stats.</p>
-      <p>Everything is done in the browser, so no data is sent to a server and the data does not persists after a page refresh. But don't use this is you have <b>any</b> privacy concerns.</p>
-      <p>Click <a href="https://faq.whatsapp.com/en/android/23756533/">here </a> to find out how to export a chat on android.</p>
-      <p><a download href="https://raw.githubusercontent.com/DWethmar/whatsapp-chat-analysis/master/public/_chat.txt">Example Whatsapp chat</a></p>
+      <p>
+        Everything is done in the browser, so no data is sent to a server and
+        the data is not stored anywhere. But don't use this if you have
+        <b>any</b> privacy concerns.
+      </p>
+      <p>
+        Click <a href="https://faq.whatsapp.com/en/android/23756533/">here </a>{" "}
+        to find out how to export a chat on android.
+      </p>
+      <p>
+        <a
+          download
+          href="https://raw.githubusercontent.com/DWethmar/whatsapp-chat-analysis/master/public/_chat.txt"
+        >
+          Example Whatsapp chat
+        </a>
+      </p>
     </header>
   );
 };
